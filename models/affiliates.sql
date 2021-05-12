6d0134b95772463c646bd9457c689795382bf951
Anton Mantulo <interstorm.bi@gmail.com>
2021-05-12 15:04:51 +0000

    Fixed sport ngr

--- models/affiliates.sql
+++ models/affiliates.sql
@@ -19,7 +19,7 @@ rmw as(SELECT userid,SUM(amounteur) as rmw_eur,CAST(endtime AS DATE) AS endtime
               GROUP BY userid,endtime),
               
 bmw as(SELECT userid,SUM(amounteur) as bmw_eur,CAST(postingcompleted AS DATE) AS postingcompleted
-             FROM {{ ref ('bmw') }}
+             FROM {{ref ('bmw') }}
              WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
               GROUP BY userid,postingcompleted),              
               
@@ -50,7 +50,7 @@ rmb AS (SELECT userid,CAST(endtime AS DATE) AS endtime, SUM(amounteur) as rmb_eu
               GROUP BY userid,endtime),
               
 bmb as(SELECT userid,SUM(amounteur) as bmb_eur,CAST(postingcompleted AS DATE) AS postingcompleted
-             FROM {{ref ('bmb') }}
+             FROM {{ref ('bmb')}}
              WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
               GROUP BY userid,postingcompleted), 
 
@@ -61,7 +61,7 @@ rmw as(SELECT userid,SUM(amounteur) as rmw_eur,CAST(endtime AS DATE) AS endtime
               GROUP BY userid,endtime),
               
 bmw as(SELECT userid,SUM(amounteur) as bmw_eur,CAST(postingcompleted AS DATE) AS postingcompleted
-             FROM {{ ref ('bmw') }}
+             FROM {{ref ('bmw')}}
              WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
               GROUP BY userid,postingcompleted),              
               
@@ -71,7 +71,7 @@ bc as (SELECT userid,SUM(amounteur) as bc_eur,CAST(postingcompleted AS DATE) AS 
               GROUP BY userid,postingcompleted), 
 
 trans AS (SELECT userid, SUM (amounteur) AS deposits, CAST (transactioncompleted AS DATE) AS transactioncompleted
-            FROM {{ ref ('transactions')}} WHERE transactiontype = 'Deposit' 
+            FROM {{ref ('transactions')}} WHERE transactiontype = 'Deposit' 
                   AND DATE(transactioncompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
                  GROUP BY userid, transactioncompleted)
                                                
@@ -84,8 +84,7 @@ SELECT joins.userid,
        User.registrationdate AS registrationdate, 
        IFNULL(trans.deposits, 0) AS deposit, 
        IFNULL(rmb_eur, 0) + IFNULL(bmb_eur, 0) AS bets,
-       IFNULL(rmb_eur, 0) + IFNULL(bmb_eur, 0) - IFNULL(rmw_eur, 0) - IFNULL(bmw_eur, 0) AS ggr,
-       IFNULL(rmb_eur, 0) + IFNULL(bmb_eur, 0) - IFNULL(rmw_eur, 0) - IFNULL(bmw_eur, 0) - IFNULL(bc_eur, 0) AS ngr       
+       IFNULL(rmb_eur, 0) + IFNULL(bmb_eur, 0) - IFNULL(rmw_eur, 0) - IFNULL(bmw_eur, 0) AS ggr,      
 FROM joins 
 FULL JOIN rmb
 ON joins.userid = rmb.userid AND joins.date = rmb.endtime 
@@ -113,7 +112,6 @@ SELECT userid,
    deposit,
    bets,
    ggr,
-   ngr,
    primary_commission,
    secondary_commission
 FROM master
@@ -130,7 +128,7 @@ WITH rmb AS (SELECT userid,CAST(endtime AS DATE) AS endtime, SUM(amounteur) as r
               
 
 bmb as(SELECT userid,SUM(amounteur) as bmb_eur,CAST(postingcompleted AS DATE) AS postingcompleted
-             FROM {{ref ('bmb') }}
+             FROM {{ref ('bmb')}}
              WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
              AND gamecode<>'OddsMatrix2'
               GROUP BY userid,postingcompleted), 
@@ -142,7 +140,7 @@ rmw as(SELECT userid,SUM(amounteur) as rmw_eur,CAST(endtime AS DATE) AS endtime
               GROUP BY userid,endtime),
               
 bmw as(SELECT userid,SUM(amounteur) as bmw_eur,CAST(postingcompleted AS DATE) AS postingcompleted
-             FROM {{ref ('bmw') }}
+             FROM {{ref ('bmw')}}
              WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
              AND gamecode<>'OddsMatrix2'
               GROUP BY userid,postingcompleted),              
@@ -210,6 +208,95 @@ FULL JOIN bmw
 ON bmw.userid = joins.userid AND bmw.postingcompleted = joins.date
 FULL JOIN bc 
 ON bc.userid = joins.userid AND bc.postingcompleted = joins.date
+ORDER BY joins.date ASC),
+
+sport AS(WITH joins AS (
+
+WITH rmb AS (SELECT B.userid,CAST(W.endtime AS DATE) as endtime,SUM(B.amounteur) as rmb_eur
+              FROM vip.BetActivity as B JOIN vip.WinActivity as W ON B.postingid=W.matchingpostingid
+              WHERE DATE(W.endtime) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE() and W.wallettype="RealCash" and B.gamecode='OddsMatrix2'
+              GROUP BY userid,endtime),
+              
+
+bmb as(SELECT userid,SUM(amounteur) as bmb_eur,CAST(postingcompleted AS DATE) AS postingcompleted
+             FROM {{ref ('bmb')}}
+             WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+             AND gamecode='OddsMatrix2'
+              GROUP BY userid,postingcompleted), 
+              
+rmw as(SELECT W.userid,CAST(W.endtime AS DATE) as endtime,SUM(W.amounteur) as rmw_eur
+              FROM vip.BetActivity as B JOIN vip.WinActivity as W ON B.postingid=W.matchingpostingid
+              WHERE DATE(W.endtime) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+              AND W.wallettype = 'RealCash' AND W.gamecode='OddsMatrix2'
+              GROUP BY userid,endtime),
+              
+bmw as(SELECT userid,SUM(amounteur) as bmw_eur,CAST(postingcompleted AS DATE) AS postingcompleted
+             FROM {{ref ('bmw')}}
+             WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+             AND gamecode='OddsMatrix2'
+              GROUP BY userid,postingcompleted),              
+              
+bc as (SELECT userid,SUM(amounteur) as bc_eur,CAST(postingcompleted AS DATE) AS postingcompleted
+             FROM {{ref ('bonus_costs')}}
+             WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE() AND type='sport'
+              GROUP BY userid,postingcompleted)
+                                          
+SELECT rmb.userid, rmb.endtime AS date
+FROM rmb
+UNION DISTINCT 
+SELECT bmb.userid, bmb.postingcompleted AS date
+FROM bmb
+UNION DISTINCT 
+SELECT rmw.userid, rmw.endtime AS date
+FROM rmw
+UNION DISTINCT 
+SELECT bmw.userid, bmw.postingcompleted AS date
+FROM bmw
+UNION DISTINCT
+SELECT bc.userid, bc.postingcompleted AS date
+FROM bc),
+
+rmb AS (SELECT B.userid,CAST(W.endtime AS DATE) as endtime,SUM(B.amounteur) as rmb_eur
+              FROM vip.BetActivity as B JOIN vip.WinActivity as W ON B.postingid=W.matchingpostingid
+              WHERE DATE(W.endtime) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE() and W.wallettype="RealCash" and B.gamecode='OddsMatrix2'
+              GROUP BY userid,endtime),
+              
+bmb as(SELECT userid,SUM(amounteur) as bmb_eur,CAST(postingcompleted AS DATE) AS postingcompleted
+             FROM {{ref ('bmb')}}
+             WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+             AND gamecode='OddsMatrix2'
+              GROUP BY userid,postingcompleted), 
+
+rmw as(SELECT W.userid,CAST(W.endtime AS DATE) as endtime,SUM(W.amounteur) as rmw_eur
+              FROM vip.BetActivity as B JOIN vip.WinActivity as W ON B.postingid=W.matchingpostingid
+              WHERE DATE(W.endtime) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+              AND W.wallettype = 'RealCash' AND W.gamecode='OddsMatrix2'
+              GROUP BY userid,endtime),
+              
+bmw as(SELECT userid,SUM(amounteur) as bmw_eur,CAST(postingcompleted AS DATE) AS postingcompleted
+             FROM {{ref ('bmw')}}
+             WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE()
+             AND gamecode='OddsMatrix2'
+              GROUP BY userid,postingcompleted),              
+              
+bc as (SELECT userid,SUM(amounteur) as bc_eur,CAST(postingcompleted AS DATE) AS postingcompleted
+             FROM {{ref ('bonus_costs')}}
+             WHERE DATE(postingcompleted) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE() AND type='sport'
+              GROUP BY userid,postingcompleted) 
+SELECT joins.userid,
+       joins.date,
+       IFNULL(rmb_eur, 0) + IFNULL(bmb_eur, 0) - IFNULL(rmw_eur, 0) - IFNULL(bmw_eur, 0) - IFNULL(bc_eur, 0) AS ngr_sport      
+FROM joins 
+FULL JOIN rmb
+ON joins.userid = rmb.userid AND joins.date = rmb.endtime 
+FULL JOIN bmb 
+ON bmb.userid = joins.userid AND joins.date = bmb.postingcompleted  
+FULL JOIN rmw 
+ON rmw.userid = joins.userid AND rmw.endtime = joins.date
+FULL JOIN bmw 
+ON bmw.userid = joins.userid AND bmw.postingcompleted = joins.date
+FULL JOIN bc 
+ON bc.userid = joins.userid AND bc.postingcompleted = joins.date
 ORDER BY joins.date ASC) 
 
 
@@ -228,10 +315,11 @@ SELECT master2.userid AS userid,
    deposit,
    bets,
    ggr,
-   ngr,
+   IFNULL(ngr_sport,0)+IFNULL(ngr_casino,0) AS ngr,
    ngr_casino,
-   IFNULL(ngr,0)-IFNULL(ngr_casino,0) AS ngr_sport,
+   ngr_sport,
    primary_commission,
    secondary_commission
 FROM master2
 FULL JOIN casino ON master2.userid=casino.userid AND master2.date=casino.date
+FULL JOIN sport ON master2.userid=sport.userid AND master2.date=sport.date
